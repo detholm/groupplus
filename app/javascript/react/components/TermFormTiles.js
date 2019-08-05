16b3bf0c347f3ff9acb0a6e7e5627ba4242d38f2
detholm <didrik.etholm@gmail.com>
2019-08-05 16:22:23 -0400

    more styling

--- app/javascript/react/components/TermFormTiles.js
+++ app/javascript/react/components/TermFormTiles.js
@@ -3,16 +3,13 @@ import React from 'react'
 const TermFormTiles = props => {
 
   return(
-    <div>
-    <label>
-    {props.term}
-    <input type="checkbox" ref={this.input} />
+  <div >
+    <label className="termTile">
+    <input type="checkbox" value="props.term" ref={this.input} />
+      {props.term}
     </label>
-
-    </div>
+  </div>
 
   )
 }
-
-
 export default TermFormTiles
