b92edf2b26fd6e888af64b91e286ed23a57103b8
detholm <didrik.etholm@gmail.com>
2019-08-08 13:21:59 -0400

    heroku

---
+++ app/javascript/react/containers/UserShowContainer.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react"
+// import PieChart from '../components/PieChart'
+import { Chart } from 'react-google-charts';
+class UserShowContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      terms: []
+    }
+  }
+
+  componentDidMount(){
+  let userID = this.props.match.params.id
+debugger
+  fetch(`/api/v1/users/${this.props.match.params.id}`)
+  .then(response => response.json())
+  .then(data => this.setState({ user: data.user, terms: data.terms}));
+
+}
+
+  render(){
+
+
+  return(
+<div className="jsxblue">
+        // <Chart
+        //   chartType="PieChart"
+        //   width="100%"
+        //   height="400px"
+        //   data={[
+        //     ['Task', 'Hours per Day'],
+        //     ['Work', 11],
+        //     ['Eat', 2],
+        //     ['Commute', 2],
+        //     ['Watch TV', 2],
+        //     ['Sleep', 7],
+        //   ]}
+        //   options={{
+        //     title: 'My Daily Activities',
+        //   }}
+        //   graph_id="PieChart"
+        // />
+
+
+</div>
+
+    )
+  }
+}
+
+export default UserShowContainer
+
+// <h1>{this.state.user.first_name} </h1>
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#index'
-
+    # root 'welcome#index'
   devise_for :users
 
   namespace :api do
